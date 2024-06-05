ea7749f84fa6512e73660b5d9099b118d341269d
lifei2586 <seely19911029@gmail.com>
2024-06-05 17:28:18 +0800

    Update index.js

--- Web/015/js/index.js
+++ Web/015/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某某某|我们|在一起吧|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||威尼斯人|我们|必将成功|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
